edad38fa1bb700cca2bf77b66d561c318ba19ca2
NotTsunami <4589807+NotTsunami@users.noreply.github.com>
2020-02-05 23:29:19 -0500

    Initial commit

---
+++ WhiteDaisies/WhiteDaisies.cs
@@ -0,0 +1,24 @@
+﻿using BepInEx;
+using RoR2;
+using UnityEngine;
+using ItemCatalog = On.RoR2.ItemCatalog;
+
+namespace Tsunami
+{
+    [BepInDependency("com.bepis.r2api")]
+    [BepInPlugin("dev.tsunami.WhiteDaisies", "WhiteDaisies", "1.0.1")]
+    public class WhiteDaisies : BaseUnityPlugin
+    {
+        public void Awake()
+        {
+            On.RoR2.ItemCatalog.RegisterItem += (orig, index, def) =>
+            {
+                if (index == ItemIndex.TPHealingNova)
+                {
+                    def.tier = ItemTier.Tier1;
+                }
+                orig(index, def);
+            };
+        }
+    }
+}
